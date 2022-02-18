aba41f5b92ca5cf8431c1857e92100d6b5db19d6
Ricky Tanudjaja CT <ricky.tanudjaja@cartrack.com>
2022-02-18 13:59:52 +0700

    fix ex 3

--- src/components/ShoeIndex/ShoeIndex.js
+++ src/components/ShoeIndex/ShoeIndex.js
@@ -45,10 +45,12 @@ const ShoeIndex = ({ sortId, setSortId }) => {
 const Wrapper = styled.div`
   display: flex;
   flex-direction: row-reverse;
+  align-items: baseline;
   gap: 32px;
 `;
 
 const LeftColumn = styled.div`
+  flex: 0 0 248px;
 `;
 
 const MainColumn = styled.div`
@@ -57,6 +59,7 @@ const MainColumn = styled.div`
 
 const Header = styled.header`
   display: flex;
+  align-items: baseline;
 `;
 
 const Title = styled.h2`
